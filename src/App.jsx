1523a8e4604043c2f48cc6c7b14d692825b5c25b
chimaobi nwaokoro <chimaobinwaokoro27@gmail.com>
2026-08-27 15:10:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'about', element: <About /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
